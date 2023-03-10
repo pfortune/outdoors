013103c78544b12f7361a6a80d75b2509b6f954f
Peter Fortune <pfortune@gmail.com>
2023-03-10 11:15:16 +0000

    updated journal side nav to add active class

--- .eleventy.js
+++ .eleventy.js
@@ -6,5 +6,5 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addWatchTarget("css");
-};
+  };
   
