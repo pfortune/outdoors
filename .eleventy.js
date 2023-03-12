db2f084e45371ddcad7fc81f8b7e73bdd1d016ea
Peter Fortune <pfortune@gmail.com>
2023-03-12 11:42:55 +0000

    removed eleventynavigation plugin, updated all templates, added simpler navigation

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,6 @@
-const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
-
-
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPlugin(eleventyNavigationPlugin);
-    eleventyConfig.addPassthroughCopy("css");
-    eleventyConfig.addPassthroughCopy("images");
-    eleventyConfig.addWatchTarget("css");
-  };
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addWatchTarget("css");
+};
   
